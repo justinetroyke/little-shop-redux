7bef8e78bd6c855e2476e59a00e01eb1c56cde9e
Manoj Panta <manojpanta95@gmail.com>
2018-05-01 19:53:01 -0600

    working on styling

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,10 @@ ActiveRecord::Schema.define(version: 2018_05_01_214628) do
   enable_extension "plpgsql"
 
   create_table "merchants", force: :cascade do |t|
-    t.integer   "merchant_id"
-    t.text      "name"
-    t.datetime  "created_at", null: false
-    t.datetime  "updated_at", null: false
+    t.integer "merchant_id"
+    t.text "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
