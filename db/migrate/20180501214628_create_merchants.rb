7967b8c6db173f1a3a7455f57b06fcb9dc5f40ae
Justine <jjtroyke@gmail.com>
2018-05-02 17:12:38 -0600

    Pass rspec merchant id required test
    We need to change the merchant migration because the id column shouldn't be named merchant_id

--- db/migrate/20180501214628_create_merchants.rb
+++ db/migrate/20180501214628_create_merchants.rb
@@ -1,10 +1,10 @@
 class CreateMerchants < ActiveRecord::Migration[5.2]
   def change
     create_table :merchants do |t|
-      t.integer     :merchant_id
       t.text        :name
+      t.integer     :merchant_id
 
-      t.timestamps  null:false
+      t.timestamps  null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_01_214628) do
+ActiveRecord::Schema.define(version: 2018_05_02_212610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "merchants", force: :cascade do |t|
+  create_table "invoices", force: :cascade do |t|
+    t.integer "customer_id"
     t.integer "merchant_id"
+    t.text "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "merchants", force: :cascade do |t|
     t.text "name"
+    t.integer "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
