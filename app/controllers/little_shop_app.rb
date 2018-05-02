62604901bade7f4685ad98647d998356a38ea4f7
Judson Stevens <Judson.Stevens@gmail.com>
2018-05-01 18:02:31 -0600

    First test running

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -1,3 +1,14 @@
 class LittleShopApp < Sinatra::Base
 
+  get '/' do
+    @merchants = Merchant.all
+
+    erb :"merchant/index"
+  end
+
+  get '/merchants' do
+    @merchants = Merchant.all
+
+    erb :"merchant/index"
+  end
 end
