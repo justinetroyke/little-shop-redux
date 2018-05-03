165774a85160e1a528dd6932c916a15ba5f0e54d
Justine <jjtroyke@gmail.com>
2018-05-03 08:26:35 -0600

    changes to seeds and spec

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -1,21 +1,9 @@
 class LittleShopApp < Sinatra::Base
 
-  get '/' do
-    @merchants = Merchant.all
-
-    erb :'merchant/index'
-  end
-
   get '/merchants' do
     @merchants = Merchant.all
 
     erb :'merchant/index'
-  end
-
-  get '/' do
-    @invoices = Invoice.all
-
-    erb :'invoice/index'
   end
 
   get '/invoices' do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,6 @@
 require 'csv'
 require File.expand_path("../../config/environment", __FILE__)
 
-merchants = CSV.open('./data/merchants.csv',
-                     headers: true,
-                     header_converters: :symbol)
-merchants.each do |merchant|
-  Merchant.create(merchant_id:  merchant[:id],
-                  name:         merchant[:name],
-                  created_at:   merchant[:created_at],
-                  updated_at:   merchant[:updated_at])
-end
-
 invoices = CSV.open('./data/invoices.csv',
                      headers: true,
                      header_converters: :symbol)
