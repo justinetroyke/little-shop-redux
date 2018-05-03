88f49339512496f2e95d17ca7f2c0491ed72f2b2
Judson Stevens <Judson.Stevens@gmail.com>
2018-05-03 08:24:44 -0600

    Change seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,4 @@
 require 'csv'
-require './app/models/merchant.rb'
-require './app/models/item.rb'
-
-merchants = CSV.open('./data/merchants.csv',
-                     headers: true,
-                     header_converters: :symbol)
-merchants.each do |merchant|
-  Merchant.create(merchant_id:  merchant[:id],
-                  name:         merchant[:name],
-                  created_at:   merchant[:created_at],
-                  updated_at:   merchant[:updated_at])
-end
 
 items = CSV.open('./data/items.csv',
                  headers: true,
