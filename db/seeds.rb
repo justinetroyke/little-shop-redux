974993269ebe8373703db38fd35076432fd3fca4
Manoj Panta <manojpanta95@gmail.com>
2018-05-05 19:05:49 -0600

    working on relating items to merchants

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,7 +38,7 @@ merchants = CSV.open('./data/merchants.csv',
                      headers: true,
                      header_converters: :symbol)
 merchants.each do |merchant|
-  Merchant.create(merchant_id:  merchant[:id],
+  Merchant.create(id:  merchant[:id],
                   name:         merchant[:name],
                   created_at:   merchant[:created_at],
                   updated_at:   merchant[:updated_at])
