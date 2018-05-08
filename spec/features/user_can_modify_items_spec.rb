e08c304ab8469a2542bb73f01702f3ae0b664e4b
Judson Stevens <Judson.Stevens@gmail.com>
2018-05-08 11:03:26 -0600

    Refactor of testing

--- spec/features/user_can_modify_items_spec.rb
+++ spec/features/user_can_modify_items_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe 'User' do
   describe 'Editing an item' do
     it 'can edit the name of an item' do
-      Merchant.create(name: "New Merchant", merchant_id: 5, created_at: "2002-11-18 00:00:00", updated_at: "2007-10-09 00:00:00")
+      Merchant.create(name: "New Merchant", id: 5, created_at: "2002-11-18 00:00:00", updated_at: "2007-10-09 00:00:00")
       Item.create(item_id: 2, merchant_id: 5, description: "New thing", name: "Newer Thing", unit_price: 15, image: 'https://i.imgur.com/iXHefKS.png', created_at: "2002-11-18 00:00:00", updated_at: "2007-10-09 00:00:00")
       Item.create(item_id: 3, merchant_id: 6, description: "Hello", name: "Nope", unit_price: 17, image: 'https://i.imgur.com/iXHefKS.png', created_at: "2002-11-18 00:00:00", updated_at: "2007-10-09 00:00:00")
       Item.create(item_id: 4, merchant_id: 7, description: "Never again", name: "Go Away", unit_price: 11, image: 'https://i.imgur.com/iXHefKS.png', created_at: "2002-11-18 00:00:00", updated_at: "2007-10-09 00:00:00")
@@ -26,7 +26,7 @@ RSpec.describe 'User' do
 
   describe 'Deleting an item' do
     it 'can delete an item' do
-      Merchant.create(name: "New Merchant", merchant_id: 5, created_at: "2002-11-18 00:00:00", updated_at: "2007-10-09 00:00:00")
+      Merchant.create(name: "New Merchant", id: 5, created_at: "2002-11-18 00:00:00", updated_at: "2007-10-09 00:00:00")
       Item.create(item_id: 2, merchant_id: 5, description: "New thing", name: "Newer Thing", unit_price: 15, image: 'https://i.imgur.com/iXHefKS.png', created_at: "2002-11-18 00:00:00", updated_at: "2007-10-09 00:00:00")
       Item.create(item_id: 3, merchant_id: 6, description: "Hello", name: "Nope", unit_price: 17, image: 'https://i.imgur.com/iXHefKS.png', created_at: "2002-11-18 00:00:00", updated_at: "2007-10-09 00:00:00")
       Item.create(item_id: 4, merchant_id: 7, description: "Never again", name: "Go Away", unit_price: 11, image: 'https://i.imgur.com/iXHefKS.png', created_at: "2002-11-18 00:00:00", updated_at: "2007-10-09 00:00:00")
@@ -46,7 +46,7 @@ RSpec.describe 'User' do
 
   describe 'Creating an item' do
     it 'can create an item' do
-      Merchant.create(name: "New Merchant", merchant_id: 5, created_at: "2002-11-18 00:00:00", updated_at: "2007-10-09 00:00:00")
+      Merchant.create(name: "New Merchant", id: 5, created_at: "2002-11-18 00:00:00", updated_at: "2007-10-09 00:00:00")
 
       visit('/')
       click_button('Items')
