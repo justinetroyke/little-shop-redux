42eb2756299f0b3838127e7e07e612c3c14db6d1
Justine <jjtroyke@gmail.com>
2018-05-07 18:19:28 -0600

    fixing

--- db/migrate/20180503221834_update_items_table_with_merchant_id.rb
+++
@@ -1,8 +0,0 @@
-class UpdateItemsTableWithMerchantId < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :merchants, :merchant_id
-    change_table :merchants do |t|
-      t.integer :id
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_07_161613) do
+ActiveRecord::Schema.define(version: 2018_05_07_172506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,12 +31,10 @@ ActiveRecord::Schema.define(version: 2018_05_07_161613) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "image"
-    t.integer "merchant_id"
   end
 
   create_table "merchants", force: :cascade do |t|
     t.text "name"
-    t.integer "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
