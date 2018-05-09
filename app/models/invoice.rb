a53427fa8ec5e39a28149367d924254efa3382b6
Justine <jjtroyke@gmail.com>
2018-05-08 23:19:58 -0600

    invoice dashboard has all info

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -11,18 +11,18 @@ class Invoice < ActiveRecord::Base
   end
 
   def self.high_unit_price
-    Invoice.find(InvoiceItem.all.sort_by(&:unit_price).reverse.first.invoice_id)
+    InvoiceItem.all.sort_by(&:unit_price).reverse.first.invoice_id
   end
 
   def self.low_unit_price
-    Invoice.find(InvoiceItem.all.sort_by(&:unit_price).first.invoice_id)
+    InvoiceItem.all.sort_by(&:unit_price).first.invoice_id
   end
 
   def self.high_qty
-    Invoice.find(InvoiceItem.all.sort_by(&:quantity).reverse.first.invoice_id)
+    InvoiceItem.all.sort_by(&:quantity).reverse.first.invoice_id
   end
 
   def self.low_qty
-    Invoice.find(InvoiceItem.all.sort_by(&:quantity).first.invoice_id)
+    InvoiceItem.all.sort_by(&:quantity).first.invoice_id
   end
 end
